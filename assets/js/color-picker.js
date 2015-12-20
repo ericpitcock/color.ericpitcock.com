5e1b6d6a7d281f753d68bb85c4321254faef10d1
Eric Pitcock <git-43p@ericpitcock.com>
2015-12-19 21:46:46 -0800

    good stuff

--- assets/js/color-picker.js
+++ assets/js/color-picker.js
@@ -16,26 +16,14 @@ $(document).ready(function(){
             
             // for each color, append a swatch
             $.each(value, function(key, value) {
-                $(id).append('
-                    <div class="col-sm-2">
-                        <div class="color-chip" style="background: ' + value.hex + '"></div>
+                $(id).append('<div class="swatch-container"><div class="swatch" style="background: ' + value.hex + '"></div></div>');
+                //console.log('this is color ' + value.hex);
+                /*
                         <h3>' + value.name + '</h3>
                         <ul class="unstyled">
                             <li><span>Hex</span><input value="' + value.hex + '" readonly></input></li>
                             <li><span>RGB</span><input value="' + value.rgb + '" readonly></input></li>
                         </ul>
-                    </div>
-                ');
-                //console.log('this is color ' + value.hex);
-                /*
-                <div class="col-sm-2">
-                <div class="color-chip" style="background: <?php echo $hexValue; ?>;"></div>
-                <h3><?php echo $colorName; ?></h3>
-                <ul class="unstyled">
-                    <li><span>Hex</span><input value="<?php echo $hexValue; ?>" readonly></input></li>
-                    <li><span>RGB</span><input value="<?php echo $RGBValue; ?>" readonly></input></li>
-                </ul>
-                </div>
                 */
             });
         });
--- functions.php
+++ functions.php
@@ -1,84 +1,34 @@
 <?php
     
-        // get the value between all the mess
-    function readBetween($content, $start, $end) {
-        $r = explode($start, $content);
-        if (isset($r[1])){
-            $r = explode($end, $r[1]);
-            return $r[0];
-        }
-        return '';
+// get the value between all the mess
+function readBetween($content, $start, $end) {
+    $r = explode($start, $content);
+    if (isset($r[1])){
+        $r = explode($end, $r[1]);
+        return $r[0];
     }
-    
-    // hex to rgb
-    function hex2rgb($hexValue) {
-        if ($hexValue[0] == '#') {
-            $hexValue = substr($hexValue, 1);
-        }
-        if (strlen($hexValue) == 6) {
-            list($r,$g,$b) = array($hexValue[0] . $hexValue[1], $hexValue[2] . $hexValue[3], $hexValue[4] . $hexValue[5]);
-        } elseif (strlen($hexValue) == 3) {
-            list($r,$g,$b) = array($hexValue[0] . $hexValue[0], $hexValue[1] . $hexValue[1], $hexValue[2] . $hexValue[2]);
-        } else {
-            return false;
-        }
-        $r = hexdec($r);
-        $g = hexdec($g);
-        $b = hexdec($b);
-        return array('red' => $r, 'green' => $g, 'blue' => $b);
-    }
-
-function output_colors() {
-    
-
-    
-    // NEW AUTO LESS FILE GRABBER SYSTEM
-    
-    // for each less file
-    foreach (glob('color/*.less') as $lessFile) {
-        
-        // split string into an array, divded by //
-        $lessContents = explode('//', file_get_contents($lessFile));
-        
-        echo '<div class="tab-pane" id="' . substr(substr(basename($lessFile), 2), 0, -5) . '">';
-
-        // for each //, make a row
-        foreach ($lessContents as $section) {
-            
-            //print_r($section);
-            
-            //echo "sections" . $section. "<br>";
-            echo '<div class="row">';
-            
-            // split section array into swatches by ";"
-            $swatches = explode(';', $section, -1);
-            
-            // output each swatch
-            foreach ($swatches as $swatch) { ?>
-                <?php
-                    $hexValue = readBetween($swatch, ': ', ';');
-                    $RGBValue = implode(', ', hex2rgb($hexValue));
-                    $colorName = str_replace('-', ' ', readBetween($swatch, '@', ':'));
-                ?>
-                
-                <div class="col-sm-2">
-                <div class="color-chip" style="background: <?php echo $hexValue; ?>;"></div>
-                <h3><?php echo $colorName; ?></h3>
-                <ul class="unstyled">
-                    <li><span>Hex</span><input value="<?php echo $hexValue; ?>" readonly></input></li>
-                    <li><span>RGB</span><input value="<?php echo $RGBValue; ?>" readonly></input></li>
-                </ul>
-                </div>
-            <? }
-                
-            echo '</div>';
-        }
-        
-        echo '</div>';
-    
-    }
+    return '';
 }
 
+// hex to rgb
+function hex2rgb($hexValue) {
+    if ($hexValue[0] == '#') {
+        $hexValue = substr($hexValue, 1);
+    }
+    if (strlen($hexValue) == 6) {
+        list($r,$g,$b) = array($hexValue[0] . $hexValue[1], $hexValue[2] . $hexValue[3], $hexValue[4] . $hexValue[5]);
+    } elseif (strlen($hexValue) == 3) {
+        list($r,$g,$b) = array($hexValue[0] . $hexValue[0], $hexValue[1] . $hexValue[1], $hexValue[2] . $hexValue[2]);
+    } else {
+        return false;
+    }
+    $r = hexdec($r);
+    $g = hexdec($g);
+    $b = hexdec($b);
+    return array('red' => $r, 'green' => $g, 'blue' => $b);
+}
+
+// process LESS files
 function printJSON() {
     // empty array for the stuff we want
     $send_array = array();
