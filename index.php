861721be28dcfbf85981dc05b1ddc773ea775152
Eric Pitcock <git-43p@ericpitcock.com>
2015-12-25 22:32:32 -0800

    new layout

--- index.php
+++ index.php
@@ -11,19 +11,23 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!--<link rel="shortcut icon" href="favicon.ico" />-->
-    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600">
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600">
     <link rel="stylesheet" href="assets/css/color-picker.css">
 </head>
 <body>
-    <header>
-        <h1><a href="/">Color Picker</a></h1>
+    <div class="controls">
+        <h1><a href="/">Colors</a></h1>
+        <h2>PRESET COLORS</h2>
         <ul class="nav nav-tabs" role="tablist"></ul>
-    </header>
-    <div class="palette">
-        <div class="clear-palette">Clear</div>
-        <p>Select colors to build your palette</p>
+        <h2>USER COLORS</h2>
     </div>
-    <div class="tab-content"></div>
+    <div class="colors">
+        <div class="palette">
+            <div class="clear-palette">Clear</div>
+            <p>Select colors to build your palette</p>
+        </div>
+        <div class="tab-content"></div>
+    </div>
     <script>
         var colors = <?php echo printJSON(); ?>;
     </script>
